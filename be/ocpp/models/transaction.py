6dd67891f94f6420918064fd539e48c49494ef31
Keith Grennan <keith@fable.co>
2023-04-03 19:30:18 -0700

    clean up transaction model

--- be/ocpp/models/transaction.py
+++ be/ocpp/models/transaction.py
@@ -6,10 +6,8 @@ from ocpp.types.stop_reason import StopReason
 
 class Transaction(models.Model):
     charge_point = models.ForeignKey(ChargePoint, on_delete=models.CASCADE)
-    started_at = models.DateTimeField(null=True)
-    stopped_at = models.DateTimeField(null=True)
-    remote_started_at = models.DateTimeField(null=True)
-    remote_stopped_at = models.DateTimeField(null=True)
+    started_at = models.DateTimeField(null=True, blank=True)
+    stopped_at = models.DateTimeField(null=True, blank=True)
     connector_id = models.CharField(max_length=64)
     id_tag = models.CharField(max_length=256)
     meter_start = models.IntegerField(default=0)
