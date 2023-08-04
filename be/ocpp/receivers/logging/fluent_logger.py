2084003f30968fd244d818112011b82fc8a0789f
Keith Grennan <keith@fable.co>
2023-08-04 10:17:26 -0700

    some fluent logging fixes

--- be/ocpp/receivers/logging/fluent_logger.py
+++ be/ocpp/receivers/logging/fluent_logger.py
@@ -7,7 +7,7 @@ from ocpp.models import WebsocketEvent, Message
 
 
 logger = (
-    sender.FluentSender("app", host=settings.FLUENTD_HOST, port=24224)
+    sender.FluentSender("ocpp", host=settings.FLUENTD_HOST, port=24224)
     if settings.FLUENTD_HOST
     else None
 )
@@ -15,13 +15,13 @@ logger = (
 
 @receiver(post_save, sender=WebsocketEvent)
 def log_websocket_events(instance: WebsocketEvent, created, **kwargs):
-    if not logger:
+    if not logger or not created:
         return
     logger.emit(f"ws.{instance.type}", dict(id=instance.charge_point.id))
 
 
 @receiver(post_save, sender=Message)
 def log_messages(instance: Message, created, **kwargs):
-    if not logger:
+    if not logger or not created:
         return
     logger.emit("message", instance.to_ocpp())
